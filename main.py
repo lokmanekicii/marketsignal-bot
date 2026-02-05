8a3b4a23e8f96629404610b1fbd2119cfaeaa9ac
lokmanekicii <lokmanekicii@gmail.com>
2026-02-06 00:04:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from telegram import Bot
 
 # ===== AYARLAR =====
-TOKEN = os.getenv("8375136572:AAGeZAaukMwqzf2KvO-TNDc65j9FSWm7fsY")
+TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL = "@marketsignal_live"
 
 SPONSOR_TEXT = "🤝 Sponsor: YourBrand | Reklam & işbirliği için DM"
